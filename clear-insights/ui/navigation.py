c2bf6c37977cd3f1c249956f6ae4ce92a2d047fd
KDataAnalytics <garytaylor2u@gmail.com>
2026-01-06 04:21:53 -0800

    made updates to navigation

--- clear-insights/ui/navigation.py
+++ clear-insights/ui/navigation.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 
+def home_icon_button() -> None:
+    col1, col2 = st.columns([12, 1])
+    with col2:
+        if st.button("Home", help="Home / Start Over"):
+            for key in list(st.session_state.keys()):
+                del st.session_state[key]
+            st.session_state.step = 0
+            st.rerun()
+
+
 def back_button(step_to_go: int) -> None:
     st.markdown("---")
     col1, col2 = st.columns([5, 1])
